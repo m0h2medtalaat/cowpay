5711806046cb624d47e20f729648b152ac5c4a4a
Andrew <andrew_samir93@hotmail.com>
2021-07-05 09:35:48 +0200

    init commit

---
+++ example/lib/credit_card_example.dart
@@ -0,0 +1,11 @@
+import 'package:cowpay/widgets/credit_card_widget.dart';
+import 'package:flutter/material.dart';
+
+class CreditCardExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: CreditCardWidget(),
+    );
+  }
+}
---
+++ lib/widgets/credit_card_widget.dart
@@ -0,0 +1,10 @@
+library cowpay;
+
+import 'package:flutter/material.dart';
+
+class CreditCardWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
