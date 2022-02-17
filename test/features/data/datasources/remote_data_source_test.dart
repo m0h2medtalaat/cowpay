8c870088e939ba70f376072ff2cebb54d373f3b6
mohamed.talat <mohamed.abdelmoniem@excelsystems-eg.com>
2022-02-17 11:57:53 +0200

    -test remote data source

---
+++ test/features/data/datasources/remote_data_source_test.dart
@@ -0,0 +1,66 @@
+import 'package:cowpay/core/constants/network/urls_data.dart';
+import 'package:cowpay/core/network/network_util.dart';
+import 'package:cowpay/features/data/datasources/remote_data_source.dart';
+import 'package:cowpay/features/data/models/fawry_request_model.dart';
+import 'package:cowpay/features/data/models/fawry_response_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'remote_data_source_test.mocks.dart';
+
+@GenerateMocks([NetworkUtil])
+void main() {
+  MockNetworkUtil mockNetworkUtil = MockNetworkUtil();
+
+  RemoteDataSourceImpl remoteDataSourceImpl =
+      RemoteDataSourceImpl(networkUtil: mockNetworkUtil);
+
+  group('getConcreteNumberTrivia', () {
+    FawryResponseModel fawryResponseModel = FawryResponseModel(
+        paymentGatewayReferenceId: "paymentGatewayReferenceId",
+        merchantReferenceId: "merchantReferenceId",
+        cowpayReferenceId: 111,
+        success: true,
+        statusCode: 200,
+        statusDescription: "200",
+        type: "fawry");
+
+    test(
+      '''should perform a GET request on a URL with number
+       being the endpoint and with application/json header''',
+      () async {
+        // arrange
+        when(
+          mockNetworkUtil.postWithRaw(UrlsData.fawryUrl,
+              body: FawryRequestModel(
+                      merchantReferenceId: "merchantReferenceId",
+                      customerMerchantProfileId: "customerMerchantProfileId",
+                      amount: "amount",
+                      signature: "signature",
+                      description: "description")
+                  .toJson()),
+        ).thenAnswer((_) async => fawryResponseModel);
+        // act
+        remoteDataSourceImpl.fawryCharge(
+            fawryRequestModel: FawryRequestModel(
+                merchantReferenceId: "merchantReferenceId",
+                customerMerchantProfileId: "customerMerchantProfileId",
+                amount: "amount",
+                signature: "signature",
+                description: "description"));
+        // assert
+        verify(mockNetworkUtil.postWithRaw(
+          UrlsData.fawryUrl,
+          body: FawryRequestModel(
+                  merchantReferenceId: "merchantReferenceId",
+                  customerMerchantProfileId: "customerMerchantProfileId",
+                  amount: "amount",
+                  signature: "signature",
+                  description: "description")
+              .toJson(),
+        ));
+      },
+    );
+  });
+}
